c2736ac4f954565fd2c6772c2a2cd415cbb0f36c
shiva <shivanandpatil.tech@gmail.com>
2019-08-28 21:09:50 +0530

    Add the session in code
    Signed-off-by: shiva <shivanandpatil.tech@gmail.com>

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const uuid = require('uuid/v4');
 
@@ -6,9 +7,20 @@ var app = express();
 
 var port = 3000;
 
+app.use(session({
+	genid: (req) => {
+		console.log("Indside the session");
+		console.log(req.sessionID);
+		return uuid();
+	},
+	secret: "test",
+	resave: false,
+	saveUninitialized: true
+}));
+
 app.get("/", function(req, res) {
-	const uniqueId = uuid();
-	res.send("Hitting the landing page. Generated uniqueId as " + uniqueId + "\n");
+	console.log("Inside req callback and sessionID is ",req.sessionID);
+	res.send("Hitting the landing page.\n");
 });
 
 app.listen(port, err => {
